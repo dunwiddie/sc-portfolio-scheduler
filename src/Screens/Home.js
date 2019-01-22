dfbce4f539dd6b20d914464cc82d99a587fa2b8e
Derek Dunwiddie <dunwiddie.derek@gmail.com>
2019-01-22 12:09:27 -0600

    Tweak CSS and begin rebuilding calendar

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -2,7 +2,7 @@ import { html } from 'lit-html';
 
 export default function Home(state, db) {
     return html`
-        <div id="home" class="navOffset">
+        <div id="home" class="">
             <h1>So?</h1>
             <p><span>Scheduler</span> is <a href="#">Derek Dunwiddie's</a> Savvy Coders Portfolio Project</p>
         </div>
